460625a89aefd8699ca2392f0a2fd1711cae1184
Djordje Vujatovic <djordje_v@hotmail.com>
2017-08-02 13:27:26 -0700

    Connected BalanceSheetContainer to Redux, mapped over necessary data to BalanceSheetComponent.

--- src/containers/BalanceSheetContainer/index.js
+++ src/containers/BalanceSheetContainer/index.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchTransactionData } from '../../redux/actions/balanceSheetActions';
+import LoadingComponent from '../../components/LoadingComponent';
 import BalanceSheetComponent from '../../components/BalanceSheetComponent';
+import BalanceSheetHeader from '../../components/BalanceSheetHeader';
 
 class BalanceSheetContainer extends Component {
+  componentDidMount() {
+    this.props.fetchTransactionData();
+  }
   render() {
+    const { isLoading } = this.props;
+    const transactionData = this.props.balanceSheet;
     return (
       <div>
-        <BalanceSheetComponent />
+        {
+          isLoading ?
+            <LoadingComponent />
+            :
+            <div>
+              <BalanceSheetHeader />
+              {transactionData.map((transaction, key) => (
+                <BalanceSheetComponent
+                  key={key}
+                  date={transaction.date}
+                  ledger={transaction.ledger}
+                  amount={transaction.amount}
+                  company={transaction.company}
+                />
+              ))}
+            </div>
+        }
       </div>
     );
   }
 }
 
-export default BalanceSheetContainer;
+const mapStateToProps = state => ({
+  isLoading: state.balanceSheetData.isLoading,
+  balanceSheet: state.balanceSheetData.data,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchTransactionData: () => {
+    dispatch(fetchTransactionData());
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BalanceSheetContainer);
