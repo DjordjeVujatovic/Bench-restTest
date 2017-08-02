7919d38ddc9dc9bfc5d9834abf054023e4c19769
Djordje Vujatovic <djordje_v@hotmail.com>
2017-08-02 13:24:52 -0700

    Looped over transaction keys, lower-cased keys.

---
+++ src/redux/reducers/balanceSheetReducer.js
@@ -0,0 +1,25 @@
+import { GET_TRANSACTION_DATA, LOADING_TRANSACTION_DATA, DONE_LOADING_TRANSACTION_DATA } from '../actions/balanceSheetActions';
+
+const initialState = { isLoading: true };
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOADING_TRANSACTION_DATA:
+      return { ...state, isLoading: true };
+    case DONE_LOADING_TRANSACTION_DATA:
+      return { ...state, isLoading: false };
+    case GET_TRANSACTION_DATA:
+      return {
+        ...state,
+        data: action.payload.transactions.map(transaction => {
+          for(var key in transaction) {
+            transaction[ key.toLowerCase() ] = transaction[key];
+            delete transaction[key];
+          }
+          return transaction;
+        })
+       };
+    default:
+      return state;
+  }
+};
